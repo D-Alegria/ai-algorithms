74859476ebfe479923b0d39b3c5cf872a46727ef
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-07-12 21:53:19 +0100

    Added enum for selecting search algorithm

--- Assets/Scripts/Core/Searcher.cs
+++ Assets/Scripts/Core/Searcher.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Core;
+using Search_Algorithms;
 using UnityEngine;
 
 public class Searcher : MonoBehaviour
@@ -8,12 +10,28 @@ public class Searcher : MonoBehaviour
     public GameObject startingNodeObject;
     private ISearch _searchStrategy;
 
+    public SearchAlgorithms searchAlgorithm;
+
     void Start()
     {
-        _searchStrategy = new DepthFirstSearch();
+        switch (searchAlgorithm)
+        {
+            case SearchAlgorithms.DepthFirstSearch:
+                _searchStrategy = new DepthFirstSearch();
+                break;
+        }
         Node startingNode = startingNodeObject != null ? startingNodeObject.GetComponent<NodeObject>()?.Node : null;
-        Debug.Log(_searchStrategy?.Search(startingNode, "E"));
+        Debug.Log(_searchStrategy?.Search(startingNode, "6"));
     }
+}
+
+public enum SearchAlgorithms{
+    DepthFirstSearch,
+    BreadthFirstSearch,
+    UniformCostSearch,
+    IterativeDeepeningSearch,
+    DepthLimitedSearch,
+    BiDirectionalSearch,
 }
 
 public interface ISearch
