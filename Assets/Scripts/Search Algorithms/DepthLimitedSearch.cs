fab920f944c30c5ab7f69d90a679c343f49b6acb
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-08-09 12:45:05 +0100

    Wrote Depth Limited Search Algorithm

--- Assets/Scripts/Search Algorithms/DepthLimitedSearch.cs
+++ Assets/Scripts/Search Algorithms/DepthLimitedSearch.cs
@@ -4,9 +4,32 @@ namespace Search_Algorithms
 {
     public class DepthLimitedSearch : ISearch
     {
-        public bool Search(Node currentNode, string targetValue)
+        private bool _found;
+        private readonly int _depthLimit;
+
+        public DepthLimitedSearch(int depthLimit)
         {
-            throw new NotImplementedException();
+            _depthLimit = depthLimit;
+        }
+
+        public bool Search(Node currentNode, String targetValue)
+        {
+            return Search(currentNode, targetValue, 0);
+        }
+
+        private bool Search(Node currentNode, String targetValue, int depth)
+        {
+            if (currentNode.Value == targetValue) return _found = true;
+            if (depth + 1 > _depthLimit) return false;
+
+            for (int i = 0; i < currentNode.Neighbors.Length; i++)
+            {
+                if (currentNode.Neighbors[i].node.Visited) continue;
+                Search(currentNode.Neighbors[i].node, targetValue, depth + 1);
+                if (_found) return _found;
+            }
+
+            return false;
         }
     }
 }
