e22264a150d899849d9d74ebb814e899e9b251e2
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-08-10 04:29:09 +0100

    Breadth First Search

---
+++ Assets/Scripts/Search Algorithms/BreadthFirstSearch.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace Search_Algorithms
+{
+    public class BreadthFirstSearch : ISearch
+    {
+        private Queue<Node> _neighbors;
+
+        public BreadthFirstSearch()
+        {
+            _neighbors = new Queue<Node>();
+        }
+
+        public BreadthFirstSearch(Queue<Node> neighbors)
+        {
+            _neighbors = neighbors;
+        }
+
+        public bool Search(Node currentNode, string targetValue)
+        {
+            if (_neighbors.Count < 1) _neighbors.Enqueue(currentNode);
+            while (_neighbors.Count > 0)
+            {
+                currentNode = _neighbors.Dequeue();
+                if (currentNode.Value == targetValue) return true;
+
+                for (int i = 0; i < currentNode.Neighbors.Length; i++)
+                {
+                    if (currentNode.Neighbors[i].node.Visited) continue;
+                    _neighbors.Enqueue(currentNode.Neighbors[i].node);
+                }
+            }
+
+            return false;
+        }
+    }
+}
