b844edf647c6d8305ed82de46fee0afba1774b8f
Oladugba Demilade <demiladeoladugba@gmail.com>
2021-08-20 16:23:02 +0100

    added feature to generate population in GeneticAlgorithm.cs

---
+++ Assets/Scripts/Genetic Algorithm/GeneticAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Core;
+
+namespace Genetic_Algorithm
+{
+    public class GeneticAlgorithm
+    {
+        private List<Chromosome> population;
+
+        public List<Chromosome> GeneratePopulation(int length)
+        {
+            List<Chromosome> chromosomes = new List<Chromosome>();
+            for (int i = 0; i < length; i++)
+            {
+                chromosomes.Add(new Chromosome().GenerateChromosome(10));
+            }
+
+            population = chromosomes;
+            return chromosomes;
+        }
+    }
+}
