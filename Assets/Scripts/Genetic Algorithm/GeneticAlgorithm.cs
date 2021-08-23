996ff282a357601d097e9568a5aff704e2b77a62
Oladugba Demilade <demiladeoladugba@gmail.com>
2021-08-23 14:12:59 +0100

    added single point crossover

--- Assets/Scripts/Genetic Algorithm/GeneticAlgorithm.cs
+++ Assets/Scripts/Genetic Algorithm/GeneticAlgorithm.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Core;
 
 namespace Genetic_Algorithm
@@ -6,6 +8,8 @@ namespace Genetic_Algorithm
     public class GeneticAlgorithm
     {
         private List<Chromosome> population;
+        private static readonly Random Random = new Random();
+        private static readonly object SyncLock = new object();
 
         public List<Chromosome> GeneratePopulation(int length)
         {
@@ -17,6 +21,26 @@ namespace Genetic_Algorithm
 
             population = chromosomes;
             return chromosomes;
+        }
+
+        public (Chromosome, Chromosome) SinglePointCrossOver(Chromosome chromosomeA, Chromosome chromosomeB)
+        {
+            if (chromosomeA.Genes.Length != chromosomeB.Genes.Length)
+            {
+                return (chromosomeA, chromosomeB);
+            }
+
+            lock (SyncLock)
+            {
+                var point = Random.Next(chromosomeA.Genes.Length);
+                var newGenesA = chromosomeA.Genes.Take(point).Concat(chromosomeB.Genes.Skip(point)).ToArray();
+                var newGenesB = chromosomeB.Genes.Take(point).Concat(chromosomeA.Genes.Skip(point)).ToArray();
+
+                chromosomeA.Genes = newGenesA;
+                chromosomeB.Genes = newGenesB;
+
+                return (chromosomeA, chromosomeB);
+            }
         }
     }
 }
