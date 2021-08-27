fa3438a3bb03972b17a05ecc208ce8d44f5b75d5
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-08-27 15:47:57 +0100

    Added a random number utility

---
+++ Assets/Scripts/Core/RandomNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Core
+{
+    public static class RandomNumber
+    {
+        private static readonly Random Generator = new Random();
+
+       public static int Generate(int maxValue)
+        {
+            return Generator.Next(maxValue);
+        }
+
+        public static int Generate(int minValue, int maxValue)
+        {
+            return Generator.Next(minValue, maxValue);
+        }
+    }
+}
